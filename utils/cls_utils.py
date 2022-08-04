fix: change cls_model softmax to sigmoid

utils/cls_utils.py
@@ -18,7 +18,7 @@ def load_yolo_model():
 # load classification model (MobileNetV2)
 def load_cls_model():
     MobileNetV2Model= tf.keras.applications.MobileNetV2(
-        input_shape=(224,224,3),
+        input_shape=(225,225,3),
         include_top=False,
         weights="imagenet",
         input_tensor=None,
@@ -29,8 +29,8 @@ def load_cls_model():
     predictions = Dense(3, activation='softmax', name = "output_node")(x)
     model = Model(inputs=MobileNetV2Model.input, outputs=predictions)
 
-    soft_model = load_model('./MobileNetV2-107-0.2315-0.9500.hdf5')
-    return soft_model
+    model = load_model('./MobileNetV2-089-0.3565-0.9250.hdf5')
+    return model
 
 # get dog location in one frame
 def get_dog_location(model, frame):